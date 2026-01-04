07f6bf7182808d1d9028939ff19dbfb1df02d922
NAGA AJAY RAGYARI <100664594+NagaAjay1812@users.noreply.github.com>
2026-01-04 15:49:21 -0500

    Add employee filtering queries in SQL
    Added SQL queries to filter employees by various criteria.

--- queries/01_basic_filters.sql
+++ queries/01_basic_filters.sql
@@ -1,1 +1,29 @@
+-- 1. List all employees earning more than 60,000
+select *
+from employees
+where salary > 60,000
 
+-- 2. List employees hired after 2022
+select *
+from employees
+where hired_date > 2022
+
+-- 3. List employees working in the IT department
+select *
+from employees
+where dept_name = 'IT'
+
+-- 4. List employees whose salary is between 60,000 and 90,000
+select *
+from employees
+where salary between 60,000 and 90,000
+
+or 
+select *
+from employees
+where salary like '60000' and salary like '90,000';
+
+-- 5. List employees whose name starts with 'A'
+select *
+from employees
+where emp_name like 'A%';
